602e1c5a855710783fbeb6ef37f23cc1d6e8b683
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-04 20:38:23 +0000

    load paaths based on list from controller

--- client/scripts/controllers/paathsCtrl.js
+++ client/scripts/controllers/paathsCtrl.js
@@ -1,4 +1,14 @@
 angular.module('sehajPaathTracker')
-.controller('paathsCtrl', function($scope) {
-
-});
+	.controller('paathsCtrl', function ($scope) {
+		$scope.paaths = [
+			{
+				title: "Family Sehaj Paath"
+			},
+			{
+				title: "Friends Sehaj Paath"
+			},
+			{
+				title: "Birthday Sehaj Paath"
+			}
+		];
+	});
