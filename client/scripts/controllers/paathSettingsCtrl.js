6e8ae63dc60f5fccbdc0fbf2657666aff4bacc4f
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-07 13:00:57 +0000

    Add ability to remove paath

--- client/scripts/controllers/paathSettingsCtrl.js
+++ client/scripts/controllers/paathSettingsCtrl.js
@@ -1,5 +1,11 @@
 angular.module('sehajPaathTracker')  
-.controller('paathSettingsCtrl', function($scope, $stateParams) {
+.controller('paathSettingsCtrl', function($scope, $stateParams, $meteor, $state) {
 	var paathId = $stateParams.paathId;
 	$scope.paath = $scope.$meteorObject(Paaths, paathId, false);
+	
+	$scope.deletePaath = function(){
+		$meteor.call("deletePaath", $scope.paath._id);
+		
+		$state.go("paaths");
+	};
 });
