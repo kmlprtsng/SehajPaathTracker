71ef56a1ace16f3d67a567fcb3c7cbf7b64b8940
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2015-12-08 18:06:42 +0000

    Added start and end ang validation

--- client/scripts/controllers/addPaathLogCtrl.js
+++ client/scripts/controllers/addPaathLogCtrl.js
@@ -1,4 +1,9 @@
 angular.module('sehajPaathTracker')
 .controller('addPaathLogCtrl', function($scope) {
-
+	
+	$scope.data = {};
+	
+	$scope.addPaathLog = function(isValid){
+		console.log("yep. Ive been submitted");
+	};
 });
