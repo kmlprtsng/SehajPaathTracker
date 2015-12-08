2f3b839dd3e25d148299870c20e3f75be22ea941
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-08 22:13:39 +0000

    add validation based on log statuses

--- client/scripts/controllers/addPaathLogCtrl.js
+++ client/scripts/controllers/addPaathLogCtrl.js
@@ -2,6 +2,7 @@ angular.module('sehajPaathTracker')
 .controller('addPaathLogCtrl', function($scope) {
 	
 	$scope.data = {};
+	$scope.paathLogStatus = PaathLogStatuses;
 	
 	$scope.addPaathLog = function(isValid){
 		console.log("yep. Ive been submitted");
