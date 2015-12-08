73cec4517f0184c45facb60b866fce61a10b30e7
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2015-12-08 12:45:18 +0000

    Fix the navigation issues with back button

--- client/scripts/controllers/loginCtrl.js
+++ client/scripts/controllers/loginCtrl.js
@@ -1,4 +1,10 @@
 angular.module('sehajPaathTracker')
-.controller('loginCtrl', function($scope) {
-
+.controller('loginCtrl', function($scope, $ionicHistory, $state) {
+	$scope.login = function(){
+		$ionicHistory.nextViewOptions({
+			historyRoot: true
+		});
+		
+		$state.go("paaths");
+	}
 });
--- client/scripts/controllers/paathSettingsCtrl.js
+++ client/scripts/controllers/paathSettingsCtrl.js
@@ -1,10 +1,15 @@
 angular.module('sehajPaathTracker')  
-.controller('paathSettingsCtrl', function($scope, $stateParams, $meteor, $state) {
+.controller('paathSettingsCtrl', function($scope, $stateParams, $meteor, $state, $ionicHistory) {
 	var paathId = $stateParams.paathId;
 	$scope.paath = $scope.$meteorObject(Paaths, paathId, false);
 	
 	$scope.deletePaath = function(){
 		$meteor.call("deletePaath", $scope.paath._id);
+		
+		$ionicHistory.nextViewOptions({
+			historyRoot: true,
+			disableAnimate: true
+		});
 		
 		$state.go("paaths");
 	};
