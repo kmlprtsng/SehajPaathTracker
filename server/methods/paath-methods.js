abad7e4c1146cfe17da975885fdebe6ada665e09
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2015-12-16 18:13:33 +0000

    refactor save paath log server method

--- server/methods/paath-methods.js
+++ server/methods/paath-methods.js
@@ -7,6 +7,7 @@
   function createPaath(paath) {
     paath.timestamp = new Date();
     paath.logs = [];
+    paath.tracking = [];
 
     var paathId = Paaths.insert(paath);
     return paathId;
--- server/methods/paathLog-methods.js
+++ server/methods/paathLog-methods.js
@@ -12,14 +12,24 @@
 
   function savePaathLog(paathId, paathLog) {
     if (paathLog._id) {
-      return Paaths.update(
-        { _id: paathId, "logs._id": paathLog._id },
-        { $set: { "logs.$": paathLog } });
+      addPaathLog(paathId, paathLog)
     }
     else {
-      var newId = new Mongo.ObjectID;
-      paathLog._id = newId._str;
-      return Paaths.update(paathId, { $push: { logs: paathLog } });
+      updatePaathLog(paathId, paathLog);
     }
   }
+  
+  function addPaathLog(paathId, paathLog){
+    return Paaths.update(
+        { _id: paathId, "logs._id": paathLog._id },
+        { $set: { "logs.$": paathLog } });
+  }
+  
+  function updatePaathLog(paathId, paathLog){
+    var newId = new Mongo.ObjectID;
+    
+    paathLog._id = newId._str;
+    return Paaths.update(paathId, { $push: { logs: paathLog } });
+  }
+  
 })();
