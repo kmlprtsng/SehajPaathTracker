bf0ecef7669cc1817a05d864e7bf3faaa71891e7
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-18 20:24:47 +0000

    protect the routes with authentication

---
+++ client/scripts/lib/authentication-interceptor.js
@@ -0,0 +1,25 @@
+(function(){
+  angular
+    .module('sehajPaathTracker')
+    .run(function ($rootScope, $state) {
+      $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
+        var isAuthenticated = Meteor.userId() !== null;
+  
+        var isAuthRequired =  toState.authRequired;
+        
+        if(isAuthRequired === undefined || isAuthRequired === null){
+          isAuthRequired = true;
+        }
+        
+        if (isAuthRequired && !isAuthenticated) {
+          $state.transitionTo("login");
+          event.preventDefault();
+        }
+        
+        if(isAuthenticated && toState.name === "login"){
+          $state.transitionTo("paaths");
+          event.preventDefault();
+        }
+      });
+    });
+})();
--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -4,47 +4,50 @@ angular.module('sehajPaathTracker')
 			.state('login', {
 				url: '/login',
 				templateUrl: 'client/templates/login.html',
-				controller: 'loginCtrl'
-			})
-			.state('signup', {
-				url: '/signup',
-				templateUrl: 'client/templates/signup.html',
-				controller: 'signupCtrl'
+				controller: 'loginCtrl',
+				authRequired: false
 			})
 			.state('paaths', {
 				url: '/paaths',
 				templateUrl: 'client/templates/paaths.html',
-				controller: 'paathsCtrl'
+				controller: 'paathsCtrl',
+				authRequired: true
 			})
 			.state('createPaath', {
 				url: '/create-paath',
 				templateUrl: 'client/templates/createPaath.html',
-				controller: "createPaathCtrl"
+				controller: "createPaathCtrl",
+				authRequired: true
 			})
 			.state('paathSettings', {
 				url: '/paaths/:paathId/settings',
 				templateUrl: 'client/templates/paathSettings.html',
-				controller: 'paathSettingsCtrl'
+				controller: 'paathSettingsCtrl',
+				authRequired: true
 			})
 			.state('paathDetails', {
 				url: '/paaths/:paathId',
 				templateUrl: 'client/templates/paathDetails.html',
-				controller: 'paathDetailsCtrl'
+				controller: 'paathDetailsCtrl',
+				authRequired: true
 			})
 			.state('paathHistoryLogs', {
 				url: '/paaths/:paathId/history-logs',
 				templateUrl: 'client/templates/paathHistoryLogs.html',
-				controller: 'paathHistoryLogsCtrl'
+				controller: 'paathHistoryLogsCtrl',
+				authRequired: true
 			})
 			.state('addPaathLog', {
 				url: '/paaths/:paathId/paath-log/add',
 				templateUrl: 'client/templates/paathLogForm.html',
-				controller: 'paathLogFormCtrl'
+				controller: 'paathLogFormCtrl',
+				authRequired: true
 			})
 			.state('editPaathLog', {
 				url: '/paaths/:paathId/paath-log/:paathLogId',
 				templateUrl: 'client/templates/paathLogForm.html',
-				controller: 'paathLogFormCtrl'
+				controller: 'paathLogFormCtrl',
+				authRequired: true
 			});
 
 		$urlRouterProvider.otherwise('/login');
