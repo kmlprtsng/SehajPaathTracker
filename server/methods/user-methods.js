8d1a69c0bd2e1bc02a61d9eecefc331a4f5e73f8
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-20 00:13:10 +0000

    fix typos

--- server/methods/user-methods.js
+++ server/methods/user-methods.js
@@ -5,12 +5,12 @@
 
   function savePostSignupDetails(profile) {
     if (!this.userId) {
-      throw new Meteor.Error('not-logged-in',
-        'Must be logged in to update users name.');
+      throw new Meteor.Error("not-logged-in",
+        "Must be logged in to update user's name.");
     }
 
     if (profile.name.length === 0) {
-      throw Meteor.Error('name-required', 'Must proive user name');
+      throw Meteor.Error('name-required', "Must provide user's name");
     }
 
     return Meteor.users.update(this.userId, { $set: { 'profile.name': profile.name } });
