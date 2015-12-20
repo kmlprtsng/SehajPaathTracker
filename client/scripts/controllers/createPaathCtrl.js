3b2ff2a4d7a4c54cc98288011cc716de89468145
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-20 12:18:15 +0000

    throw error on invalid email

--- client/scripts/controllers/createPaathCtrl.js
+++ client/scripts/controllers/createPaathCtrl.js
@@ -1,7 +1,7 @@
 angular.module('sehajPaathTracker')
 	.controller('CreatePaathCtrl', CreatePaathController);
 
-function CreatePaathController($scope, $state) {
+function CreatePaathController($scope, $state, $ionicPopup) {
 	var vm = this;
 
 	vm.data = {
@@ -10,12 +10,14 @@ function CreatePaathController($scope, $state) {
 	};
 
 	vm.createPaath = createPaath;
+	vm.addPerson = addPerson;
+
 	vm.people = [{
 		name: "Jasvinder Kaur"
 	},
-	{
-		name: "Satvinder Kaur"
-	}];
+		{
+			name: "Satvinder Kaur"
+		}];
 
 	$scope.$watch("vm.data.title", function () {
 		vm.data.formValid = !(_.isEmpty(vm.data.title));
@@ -34,4 +36,23 @@ function CreatePaathController($scope, $state) {
 
 		$state.go('paaths');
 	};
+
+	function addPerson() {
+		if (vm.addPeopleForm.email.$error.email) {
+			$ionicPopup.alert({
+				title: "Invalid Email",
+				template: '<center>Please enter valid email pyario !!</center>'
+			});
+		}
+		
+		//validate user exists
+		
+		//if user doesn't exist then throw alert
+		
+		//add to the list of group
+		
+		//save users into the db
+		
+		//if user already added then handle that scenario
+	}
 };
