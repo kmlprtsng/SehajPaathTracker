922a5d69e96533c84124d597b96d8731559fecfe
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-21 11:51:16 +0000

    add name filter to show name appropriately.

---
+++ client/scripts/filters/name.filter.js
@@ -0,0 +1,19 @@
+angular
+    .module("sehajPaathTracker")
+    .filter("name", nameFilter);
+    
+function nameFilter(){
+    return function(user){
+        if (!user) return;
+        
+        var loggedInUser = Meteor.user();
+        
+        if(user._id === loggedInUser._id){
+            return "You";
+        }
+        
+        let hasName = user && user.profile && user.profile.name;
+        
+        return hasName ? user.profile.name : "No Name";
+    };
+}
