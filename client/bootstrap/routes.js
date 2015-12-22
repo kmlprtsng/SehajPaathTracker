47cffd67902cd9a855e8b6522558f097b2d1565e
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-22 15:08:22 +0000

    split client folders by feature

--- client/bootstrap/routes.js
+++ client/bootstrap/routes.js
@@ -5,42 +5,42 @@ angular.module('sehajPaathTracker')
 		$stateProvider
 			.state('login', {
 				url: '/login',
-				templateUrl: 'client/templates/login.html',
+				templateUrl: 'client/user-accounts/login.html',
 				authRequired: false
 			})
 			.state('paaths', {
 				url: '/paaths',
-				templateUrl: 'client/templates/paaths.html',
+				templateUrl: 'client/paath/list/paaths.html',
 				controller: 'PaathsCtrl as vm',
 				authRequired: true
 			})
 			.state('createPaath', {
 				url: '/create-paath',
-				templateUrl: 'client/templates/createPaath.html',
+				templateUrl: 'client/paath/create/create-paath.html',
 				controller: "CreatePaathCtrl as vm",
 				authRequired: true
 			})
 			.state('paathSettings', {
 				url: '/paaths/:paathId/settings',
-				templateUrl: 'client/templates/paathSettings.html',
+				templateUrl: 'client/paath/settings/paath-settings.html',
 				controller: 'PaathSettingsCtrl as vm',
 				authRequired: true
 			})
 			.state('paathDetails', {
 				url: '/paaths/:paathId',
-				templateUrl: 'client/templates/paathDetails.html',
+				templateUrl: 'client/paath/details/paath-details.html',
 				controller: 'PaathDetailsCtrl as vm',
 				authRequired: true
 			})
 			.state('paathHistoryLogs', {
 				url: '/paaths/:paathId/history-logs',
-				templateUrl: 'client/templates/paathHistoryLogs.html',
+				templateUrl: 'client/paath/history/paath-history-logs.html',
 				controller: 'PaathHistoryLogsCtrl as vm',
 				authRequired: true
 			})
 			.state('addPaathLog', {
 				url: '/paaths/:paathId/paath-log/add',
-				templateUrl: 'client/templates/paathLogForm.html',
+				templateUrl: 'client/paath/log/paath-log-form.html',
 				controller: 'PaathLogFormCtrl as vm',
 				authRequired: true,
 				resolve: {
@@ -49,7 +49,7 @@ angular.module('sehajPaathTracker')
 			})
 			.state('editPaathLog', {
 				url: '/paaths/:paathId/paath-log/:paathLogId',
-				templateUrl: 'client/templates/paathLogForm.html',
+				templateUrl: 'client/paath/log/paath-log-form.html',
 				controller: 'PaathLogFormCtrl as vm',
 				authRequired: true,
 				resolve: {
