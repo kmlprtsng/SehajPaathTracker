5aa2df54b18ed8da36858290908d0ea8b9230d90
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2015-12-23 19:46:07 +0000

    show users for paath

--- client/paath/settings/paath-settings.controller.js
+++ client/paath/settings/paath-settings.controller.js
@@ -1,30 +1,42 @@
 angular.module('sehajPaathTracker')
-	.controller('PaathSettingsCtrl', PaathSettingsController);
+    .controller('PaathSettingsCtrl', PaathSettingsController);
 
 function PaathSettingsController($scope, $stateParams, $state, $ionicHistory, $reactive) {
-	$reactive(this).attach($scope);
+    $reactive(this).attach($scope);
 
-	var vm = this,
-	 	paathId = $stateParams.paathId;
+    var vm = this,
+        paathId = $stateParams.paathId;
 
-	vm.helpers({
-		paath() { 
-			return Paaths.findOne(paathId); 
-		} 
-	});
-	
-	vm.deletePaath = deletePaath;
-	
-	////////////
+    vm.users = [];
 
-	function deletePaath() {
-		Meteor.call("deletePaath", vm.paath._id);
+    vm.subscribe('users');
 
-		$ionicHistory.nextViewOptions({
-			historyRoot: true,
-			disableAnimate: true
-		});
+    vm.helpers({
+        paath() {
+            return Paaths.findOne(paathId);
+        }
+    });
 
-		$state.go("paaths");
-	}
+    var paathWatch = $scope.$watch("vm.paath", function () {
+        if (vm.paath) {
+            vm.users = Meteor.users.find({ _id: { $in: vm.paath.users } }).fetch();
+            paathWatch();
+        }
+    });
+
+    vm.deletePaath = deletePaath;
+    vm.addUser = function () { };
+    
+    ////////////
+
+    function deletePaath() {
+        Meteor.call("deletePaath", vm.paath._id);
+
+        $ionicHistory.nextViewOptions({
+            historyRoot: true,
+            disableAnimate: true
+        });
+
+        $state.go("paaths");
+    }
 }
