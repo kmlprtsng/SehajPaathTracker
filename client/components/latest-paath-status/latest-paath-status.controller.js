1104209a488e3b29a1ecd5fcf31a5aeb4e471d23
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2016-01-08 21:40:01 +0000

    refactor latest paath status code into service

--- client/components/latest-paath-status/latest-paath-status.controller.js
+++ client/components/latest-paath-status/latest-paath-status.controller.js
@@ -2,27 +2,34 @@ angular
     .module("sehajPaathTracker")
     .controller("LatestPaathStatusCtrl", LatestPaathStatusController);
 
-function LatestPaathStatusController($scope, stringHelper) {
+function LatestPaathStatusController($scope, userPaathStatus) {
     var vm = this,
         userLogs = _.where(vm.paathLogs, { userId: vm.user._id }),
-        latestLog,
-        loggedInUserId = Meteor.user()._id;
-
-    if (userLogs.length > 0) {
-        latestLog = findInProgressLatestLog(userLogs);
-
-        if (!latestLog) {
-            latestLog = findLastUpdatedLog(userLogs);
-        }
-    }
-
+        latestLog = getLatestPaathLog();
+    
     $scope.$watch("latestLog", function () {
         if (latestLog) {
-            vm.latestLogText = getLatestLogText();
+            vm.latestLogText = userPaathStatus.getLogStatusText(vm.user, latestLog);
         }
     });
     
     /////////////////
+    function getLatestPaathLog() {
+        var foundLatestLog;
+        
+        if (userLogs.length === 0) {
+            return null;
+        }
+        
+        foundLatestLog = findInProgressLatestLog(userLogs);
+
+        if (!foundLatestLog) {
+            foundLatestLog = findLastUpdatedLog(userLogs);
+        }
+        
+        return foundLatestLog;
+    }
+
     function findInProgressLatestLog(userLogs) {
         return _.find(userLogs, function (log) {
             return log.status === PaathLogStatuses.inProgress;
@@ -33,35 +40,5 @@ function LatestPaathStatusController($scope, stringHelper) {
         return _.max(userLogs, function (log) {
             return log.updatedDate;
         });
-    }
-
-    function getLatestLogText() {
-        var hasHaveStr = loggedInUserId === vm.user._id ? "have" : "has";
-
-        if (!latestLog) {
-            return stringHelper.format("{hasHave} not started any angs yet", { hasHave: hasHaveStr });
-        }
-
-        var latestText;
-
-        switch (latestLog.status) {
-            case PaathLogStatuses.inProgress.title:
-                latestText = "{hasHave} in progress ang {startAng} to {finishAng}";
-                break;
-            case PaathLogStatuses.willDo.title:
-                latestText = "{hasHave} assigned ang {startAng} to {finishAng}";
-                break;
-            case PaathLogStatuses.done.title:
-                latestText = "{hasHave} completed ang {startAng} to {finishAng}";
-                break;
-            default: return "Uknown log status";
-        }
-
-        return stringHelper.format(latestText,
-            {
-                hasHave: hasHaveStr,
-                startAng: latestLog.startAng,
-                finishAng: latestLog.finishAng
-            });
     }
 }
---
+++ client/components/latest-paath-status/user-paath-status.service.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('sehajPaathTracker')
+        .service('userPaathStatus', userPaathStatusService);
+
+    userPaathStatusService.$inject = ['stringHelper'];
+
+    function userPaathStatusService(stringHelper) {
+        this.getLogStatusText = getLogStatusText;
+        
+        ////////////////
+        function getLogStatusText(user, latestLog) {
+            
+            var loggedInUserId = Meteor.user()._id,
+                hasHaveStr = loggedInUserId === user._id ? "have" : "has";
+
+            if (!latestLog) {
+                return stringHelper.format("{hasHave} not started any angs yet", { hasHave: hasHaveStr });
+            }
+
+            var latestText;
+
+            switch (latestLog.status) {
+                case PaathLogStatuses.inProgress.title:
+                    latestText = "{hasHave} in progress ang {startAng} to {finishAng}";
+                    break;
+                case PaathLogStatuses.willDo.title:
+                    latestText = "{hasHave} assigned ang {startAng} to {finishAng}";
+                    break;
+                case PaathLogStatuses.done.title:
+                    latestText = "{hasHave} completed ang {startAng} to {finishAng}";
+                    break;
+                default: return "Uknown log status";
+            }
+
+            return stringHelper.format(latestText,
+                {
+                    hasHave: hasHaveStr,
+                    startAng: latestLog.startAng,
+                    finishAng: latestLog.finishAng
+                });
+        }
+    }
+})();
