e257fbc711c01f50ec4eae80e7e3b0b1ba09a83e
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2016-02-03 13:02:07 +0000

    missing angs directive created

---
+++ client/components/missing-angs/missing-angs.controller.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+    angular
+        .module('sehajPaathTracker')
+        .controller('MissingAngsCtrl', MissngAngsController);
+
+    // ControllerController.$inject = ['dependency1'];
+    function MissngAngsController() {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
