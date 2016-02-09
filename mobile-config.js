df38edfe967434eee3d12c66828bc7c35656fe38
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2016-02-09 17:39:44 +0000

    add mobile config

---
+++ mobile-config.js
@@ -0,0 +1,15 @@
+App.info({
+  name: 'SehajPaathTracker',
+  description: 'An iOS app built with Meteor',
+  version: '0.0.1'
+});
+
+App.icons({
+  'iphone': 'resources/icons/icon-72.png',
+  'iphone_2x': 'resources/icons/icon-72@2x.png'
+});
+
+App.launchScreens({
+  'iphone': 'resources/splash/Default~iphone.png',
+  'iphone_2x': 'resources/splash/Default@2x~iphone_640x960.png'
+});
