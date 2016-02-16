d02310d5ffdd01d64073f76d438267a625d0d1f9
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2016-02-16 22:42:53 +0000

    remove redunant code

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,6 +1,2 @@
 Meteor.startup(function () {
-	WebApp.connectHandlers.use(function (req, res, next) {
-        res.setHeader('access-control-allow-origin', '*');
-        return next();
-      });
 });
